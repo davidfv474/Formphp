c200b1ce627973a3e71ac37716dc5bf2e0746336
davidfv474 <davidfontechavargas3@gmail.com>
2024-11-24 20:45:45 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
-
+<?php
+$Identificacion=$_POST ['nomApellidos'];
+$nomApellidos=$_POST['nomApellidos' ];
+$Cargo=$_POST['Cargo'];
+$Salario=$_POST['Salario'];
+echo" la identificacion del Empleado es:". Identificacion;
+echo"Los nombres y apellidos del empleado son:". nomApellidos;
+echo"El cargo del esmpleado es:". Cargo;
+echo"El salario del empleado es:". Salario;
+?>
